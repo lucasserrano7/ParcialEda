db8886f7c9fcf9fe534e529bde02d873becd4e4c
emanuelamaya200-source <emanuelamaya200@gmail.com>
2026-06-04 18:41:22 -0300

    nodo

--- src/parcialeda/Nodo.java
+++ src/parcialeda/Nodo.java
@@ -9,5 +9,31 @@ package parcialeda;
  * @author Ema
  */
 public class Nodo {
+    Sala sala;
+    
+    Nodo Siguiente;
+    Nodo Siguiente2;
+    Nodo Siguiente3;
+
+    //constructor solo para un nodo siguiente
+    public Nodo(Sala sala, Nodo Siguiente) {
+        this.sala = sala;
+        this.Siguiente = Siguiente;
+    }
+    //constructor para dos nodos siguientes
+    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2) {
+        this.sala = sala;
+        this.Siguiente = Siguiente;
+        this.Siguiente2 = Siguiente2;
+    }
+    //constructor para 3 nodos siguientes
+    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2, Nodo Siguiente3) {
+        this.sala = sala;
+        this.Siguiente = Siguiente;
+        this.Siguiente2 = Siguiente2;
+        this.Siguiente3 = Siguiente3;
+    }
+    
+    
     
 }
