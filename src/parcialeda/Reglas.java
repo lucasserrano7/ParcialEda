79bef542a94e9001ee2ec0c9d13984e9d375863f
Lucas <serranolucas1000@gmail.com>
2026-06-04 18:40:11 -0300

    Clase reglas
    aun sin implementar reglas

---
+++ src/parcialeda/Reglas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parcialeda;
+
+/**
+ *
+ * @author user
+ */
+public class Reglas {
+    public static void reglas(){
+        System.out.println("Reglas del juego: ");
+    } 
+}
