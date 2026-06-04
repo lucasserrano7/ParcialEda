76a7fca61ec971ebbe42077673c64fb7874f869e
emanuelamaya200-source <emanuelamaya200@gmail.com>
2026-06-04 19:16:17 -0300

    nodo
    ahora el nodo muestra las clases a las que se puede avanzar

--- src/parcialeda/Nodo.java
+++ src/parcialeda/Nodo.java
@@ -1,32 +1,73 @@
-
 import parcialeda.Sala;
 
 public class Nodo {
-    Sala sala;
-    
-    Nodo Siguiente;
-    Nodo Siguiente2;
-    Nodo Siguiente3;
+    private Sala sala;
+    private Nodo[] siguiente;
 
-    //constructor solo para un nodo siguiente
-    public Nodo(Sala sala, Nodo Siguiente) {
+    public Nodo(Sala sala, Nodo siguiente1) {
         this.sala = sala;
-        this.Siguiente = Siguiente;
+        // se crea un nodo de tamaño uno
+        this.siguiente = new Nodo[1];
+        this.siguiente[0] = siguiente1;
     }
-    //constructor para dos nodos siguientes
-    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2) {
+
+   
+    public Nodo(Sala sala, Nodo siguiente1, Nodo siguiente2) {
         this.sala = sala;
-        this.Siguiente = Siguiente;
-        this.Siguiente2 = Siguiente2;
+        //dos
+        this.siguiente = new Nodo[2]; 
+        this.siguiente[0] = siguiente1;
+        this.siguiente[1] = siguiente2;
     }
-    //constructor para 3 nodos siguientes
-    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2, Nodo Siguiente3) {
+
+
+    public Nodo(Sala sala, Nodo siguiente1, Nodo siguiente2, Nodo siguiente3) {
         this.sala = sala;
-        this.Siguiente = Siguiente;
-        this.Siguiente2 = Siguiente2;
-        this.Siguiente3 = Siguiente3;
+        //tres
+        this.siguiente = new Nodo[3]; 
+        this.siguiente[0] = siguiente1;
+        this.siguiente[1] = siguiente2;
+        this.siguiente[2] = siguiente3;
     }
-    //comentario
-    
-    
+
+    public void opcionesDeAvanzar() {
+        int salas = 0;
+        
+        for (Nodo siguiente : this.siguiente) {
+            // se verifica que no sea nulo tanto el sig como la sala
+            if (siguiente != null && siguiente.getSala()!= null) {
+                salas++;
+            }
+        }
+
+        // cartel para avanzar
+        if (salas > 0) {
+            System.out.println("Tenes " + salas + " salas para avanzar:");
+            
+            for (Nodo siguiente : this.siguiente) {
+                if (siguiente != null && siguiente.getSala() != null) {
+                    System.out.println("- Sala: " + siguiente.getSala().getNumeroSala());
+                }
+            }
+        } else {
+            System.out.println("No hay salas disponibles para avanzar (¡Llegaste al final!).");
+        }
+    }
+
+    // Getters y Setters
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public Nodo[] getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo[] siguiente) {
+        this.siguiente = siguiente;
+    }
 }
