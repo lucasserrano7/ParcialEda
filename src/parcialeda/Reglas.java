eeb20107109f98915800cc8dec777fd37d819663
emanuelamaya200-source <emanuelamaya200@gmail.com>
2026-06-04 18:46:37 -0300

    nodo

--- src/parcialeda/Reglas.java
+++ src/parcialeda/Reglas.java
@@ -14,32 +14,4 @@ public class Reglas {
         System.out.println("Reglas del juego: ");
     } 
 
-public class Nodo {
-    Sala sala;
-    
-    Nodo Siguiente;
-    Nodo Siguiente2;
-    Nodo Siguiente3;
 
-    //constructor solo para un nodo siguiente
-    public Nodo(Sala sala, Nodo Siguiente) {
-        this.sala = sala;
-        this.Siguiente = Siguiente;
-    }
-    //constructor para dos nodos siguientes
-    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2) {
-        this.sala = sala;
-        this.Siguiente = Siguiente;
-        this.Siguiente2 = Siguiente2;
-    }
-    //constructor para 3 nodos siguientes
-    public Nodo(Sala sala, Nodo Siguiente, Nodo Siguiente2, Nodo Siguiente3) {
-        this.sala = sala;
-        this.Siguiente = Siguiente;
-        this.Siguiente2 = Siguiente2;
-        this.Siguiente3 = Siguiente3;
-    }
-    //comentario
-    
-    
-}
